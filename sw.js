b1283a0091b99f11459e8523f37e21558b106db0
Germila SAINT-JEAN <germilasaintjean@gmail.com>
2020-10-28 16:14:37 -0400

    the Service Worker.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// sw.js
-importScripts('swenv.js'); // this file should have all the vars declared
-console.log(process.env.local.VUE_APP_FCM_SENDER_ID);
+// // sw.js
+// importScripts('swenv.js'); // this file should have all the vars declared
+// console.log(process.env.local.VUE_APP_FCM_SENDER_ID);
